d585b239f85947547d04c0197c19eb594c2e13e1
cockochan <56898295+cockochan@users.noreply.github.com>
2020-06-06 01:18:39 +0300

    ehhh

--- src/App.js
+++ src/App.js
@@ -7,13 +7,27 @@ import AllCountries from './AllCountries.js'
 function App() {
   allCountryScores.sort((a, b) => a.name.localeCompare(b.name))
   const [scoresToRender, setScoresToRender]=useState(allCountryScores)
+  const [reversible, setReversible]=useState(allCountryScores)
   const handleSearch = (event)=>{
-    console.log(event.target.value)
+
     const filteredCountry = allCountryScores.filter(country=>country.name.toLowerCase().includes(event.target.value.toLowerCase()))
     setScoresToRender(filteredCountry.sort((a, b) => a.name.localeCompare(b.name)))
   }
+  const reverse = (event)=>{
+
+    setReversible(reversible.reverse())
+  
+    setScoresToRender(reversible)
+    console.log(reversible)
+  }
+const Superbutton =()=>{
+  return(
+  <button onClick={reverse}>Sort inverse</button>
+  )
+}
   return (
     <div className="blueColumn">
+      <Superbutton />
       <Search handleSearch={handleSearch}/>
       <p className="allHighs">High Scores per Country</p>
        <AllCountries allCountryScores={scoresToRender}/>
